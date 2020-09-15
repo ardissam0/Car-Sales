7db4ddb5161caa320f773ea4b9eec524dd51dd99
Sam Ardis <ardis.productions@gmail.com>
2020-09-14 22:26:30 -0400

    Added new stlying, and extra features to buy

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -12,7 +12,12 @@ const initialState = {
       { id: 1, name: 'V-6 engine', price: 1500 },
       { id: 2, name: 'Racing detail package', price: 1500 },
       { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
+      { id: 4, name: 'Performance brakes', price: 300 },
+      { id: 4, name: 'Bulletproof body', price: 20000 },
+      { id: 4, name: 'Solar roof', price: 1000 },
+      { id: 4, name: 'Racing tires', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 },
+      { id: 4, name: 'Matt paint', price: 350 }
     ]
   };
 
